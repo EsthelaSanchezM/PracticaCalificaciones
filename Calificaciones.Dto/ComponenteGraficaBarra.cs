ff761f46874e0f5d85a435317f0f20e252c27c41
EsthelaSanchezM <44651600+EsthelaSanchezM@users.noreply.github.com>
2018-10-31 13:55:02 -0700

    Add files via upload

---
+++ Calificaciones.Dto/ComponenteGraficaBarra.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calificaciones.Dto
+{
+    public class ComponenteGraficaBarra
+    {
+        public int EjeY { get; set; }
+
+        public decimal EjeX { get; set; }
+
+        public string Color { get; set; }
+    }
+}
---
+++ Calificaciones.Dto/Estudiante.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calificaciones.Dto
+{
+    public class Estudiante
+    {
+        public string Nombres { get; set; }
+
+        public string ApellidoPaterno { get; set; }
+
+        public string ApellidoMaterno { get; set; }
+
+        public int Grado { get; set; }
+
+        public string Grupo { get; set; }
+
+        public decimal Calificacion { get; set; }
+    }
+}
